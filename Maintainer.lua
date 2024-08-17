787bec1143ab0fdf30fd3bbc25c5ec4bc94704d7
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-08-17 17:24:34 +0200

    Add threshold to requestItem call

--- Maintainer.lua
+++ Maintainer.lua
@@ -1,22 +1,22 @@
 local ae2 = require("src.AE2")
 local cfg = require("config")
-
+ 
 local items = cfg.items
 local sleepInterval = cfg.sleep
-
+ 
 while true do
     local itemsCrafting = ae2.checkIfCrafting()
-
-    for k, v in pairs(items) do
+ 
+    for item, config in pairs(items) do
         if itemsCrafting[k] ~= true then
-            local answer = ae2.requestItem(k, v)
+            local answer = ae2.requestItem(item, config[1], config[2])
             if answer == true then
-                print("[" .. os.date("%H:%M:%S") .. "] " .. "Requested " .. v .. "x " .. k)
+                print("[" .. os.date("%H:%M:%S") .. "] " .. "Requested " .. item .. "x " .. config[2])
             else
-                print("[" .. os.date("%H:%M:%S") .. "] " .. "Failed to request " .. v .. "x " .. k)
+                print("[" .. os.date("%H:%M:%S") .. "] " .. "Failed to request " .. item .. "x " .. config[2])
             end
         end
-
+ 
     end
     os.sleep(sleepInterval)
 end

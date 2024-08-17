8ab94bb65f4a7c502d9fa7a5b4af90c5c4544d37
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-08-17 17:25:44 +0200

    Add threshold to requestItem function
    Aborts request if item count in system exceeds threshold

--- src/AE2.lua
+++ src/AE2.lua
@@ -3,13 +3,20 @@ local ME = component.me_interface
 
 local AE2 = {}
 
-function AE2.requestItem(name, count)
+function AE2.requestItem(name, threshold, count)
     craftables = ME.getCraftables({
         ["label"] = name
     })
 
     if #craftables >= 1 then
         item = craftables[1].getItemStack()
+        itemInSystem = ME.getItemsInNetwork({
+        	["label"] = name        
+        })
+        if (#itemInSystem > 0 and itemInSystem[1]["size"] > threshold) then 
+            print("The amount of " .. itemInSystem[1]["label"] .. " exceeds threshold! Aborting request.")
+            return false
+        end
         if item.label == name then
             local craft = craftables[1].request(count)
 

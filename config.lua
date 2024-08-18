393bb9548ca52d3660f9cb6c183ccb9c83366866
Niels <hermsen.niels@gmail.com>
2024-08-18 19:23:26 +0200

    fix example

--- config.lua
+++ config.lua
@@ -2,13 +2,13 @@ local cfg = {}
 
 -- EXAMPLE --
 
--- [item_name] = {batch_size, threshold (if no threshold is present it maintains to inf)} -- keep in mind that no threshold has a better performance!
--- ["Osmium Dust"] = {64} -- without threshold, batch_size=64
--- ["drop of Molten SpaceTime"] = {1, 1000000} -- with threshold
+-- [item_name] = {threshold, batch_size} -- keep in mind that no threshold has a better performance!
+-- ["Osmium Dust"] = {nil, 64} -- without threshold, batch_size=64
+-- ["drop of Molten SpaceTime"] = {1000000, 1} -- with threshold
 
 cfg["items"] = {
-    ["drop of Molten SpaceTime"] = {1}
-    ["drop of Molten White Dwarf Matter"] = {1}
+    ["drop of Molten SpaceTime"] = {nil, 1}
+    ["drop of Molten White Dwarf Matter"] = {nil, 1}
 }
 
 cfg["sleep"] = 10
